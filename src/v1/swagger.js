f29f55d3429483645667cfd85e7cc73a172bf62e
Sliman <61917837+Slimattcode@users.noreply.github.com>
2022-06-29 21:04:31 +0200

    Update swagger.js

--- src/v1/swagger.js
+++ src/v1/swagger.js
@@ -7,7 +7,7 @@ const swaggerUi = require("swagger-ui-express");
 const options = {
   definition: {
     openapi: "3.0.0",
-    info: { title: "Crossfit WOD API", version: "1.0.0" },
+    info: { title: "Fitness API", version: "1.0.0" },
   },
   apis: [
     "./src/v1/routes/workoutRoutes.js",
